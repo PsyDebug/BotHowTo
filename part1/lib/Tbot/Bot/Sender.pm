0b22ed9d87cce8863e643421fee83845830f04df
PsyDebug <spryatalso@gmail.com>
2018-02-04 16:51:07 +0300

    Update Sender.pm

--- part1/lib/Tbot/Bot/Sender.pm
+++ part1/lib/Tbot/Bot/Sender.pm
@@ -9,6 +9,7 @@ sub new { bless {}, shift }
 
 sub send_msg {
     my ($class,$self,$id,$text)=@_;
+    $text=$text || "try later";
     my $url=$api_url.$self->confapp->{config}->{token}."/sendMessage?chat_id=$id&text=$text";
     my $req=$self->ua->get($url);
     if (my $err=$req->error) {$self->loger->error("[send_msg] $url", "$err->{code} response: $err->{message}");}
